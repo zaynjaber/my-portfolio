b5a14971ff6f78cbea8be68a847ca62abc6e163f
zaynjaber <zynjaber@gmail.com>
2024-07-17 06:24:56 -0400

    v1.0
    Launching the website

---
+++ js/app.js
@@ -0,0 +1,135 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+particlesJS(
+  "particles-js",
+
+  {
+    particles: {
+      number: {
+        value: 48,
+        density: {
+          enable: true,
+          value_area: 400.851810096455,
+        },
+      },
+      color: {
+        value: ["#ffffff", "#EF033C", "#3F79AB"],
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000",
+        },
+        polygon: {
+          nb_sides: 5,
+        },
+        image: {
+          src: "img/github.svg",
+          width: 100,
+          height: 100,
+        },
+      },
+      opacity: {
+        value: 1,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 7.8321678321678325,
+          opacity_min: 0.08791208791208792,
+          sync: false,
+        },
+      },
+      size: {
+        value: 1,
+        random: true,
+        anim: {
+          enable: true,
+          speed: 4.385614385614385,
+          size_min: 0.7992007992007992,
+          sync: false,
+        },
+      },
+      line_linked: {
+        enable: true,
+        distance: 25,
+        color: "#ffffff",
+        opacity: 0.5129469790382244,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 1,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "grab",
+        },
+        onclick: {
+          enable: true,
+          mode: "push",
+        },
+        resize: true,
+      },
+      modes: {
+        grab: {
+          distance: 101.92807192807193,
+          line_linked: {
+            opacity: 0.5,
+          },
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+        push: {
+          particles_nb: 4,
+        },
+        remove: {
+          particles_nb: 2,
+        },
+      },
+    },
+    retina_detect: true,
+    config_demo: {
+      hide_card: false,
+      background_color: "#b61924",
+      background_image: "",
+      background_position: "50% 50%",
+      background_repeat: "no-repeat",
+      background_size: "cover",
+    },
+  }
+);
